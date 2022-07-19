9dcaddb098de246c90737495ad5bb2edd32d24e1
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-07-18 21:56:05 -0300

    subindo resposta final com resposta exercicio 3

--- src/exercicio03/MainCardapio.java
+++ src/exercicio03/MainCardapio.java
@@ -1,14 +1,50 @@
 package exercicio03;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainCardapio {
 
     public static void main(String[] args) {
 
+        ArrayList<Prato> prato = new ArrayList<>();
 
+        boolean c = true;
 
+        Scanner opcoes = new Scanner(System.in);
+
+        Prato tipo = new Prato(null, null);
+
+        while (c == true) {
+            System.out.println();
+            System.out.println("Digite o que deseja fazer: ");
+            System.out.println("1 = Adicionar Produto: ");
+            System.out.println("2 = Exibir Lista Produtos: ");
+            System.out.println("3 = Excluir Produto: ");
+            System.out.println("4 = Encerrar o programa: ");
+
+            int oQueEscolheu = opcoes.nextInt();
+
+            switch (oQueEscolheu){
+                case 1:
+                    tipo.adicionarPrato();
+                    break;
+
+                case 2:
+                    tipo.exibirPratos();
+                    break;
+
+                case 3:
+                    tipo.excluirPrato();
+                    break;
+
+                case 4:
+                    c = false;
+
+            }
+        }
 
     }
-
-
 
 }
--- src/exercicio03/Prato.java
+++ src/exercicio03/Prato.java
@@ -5,40 +5,66 @@ import java.util.Scanner;
 
 public class Prato {
 
-    String nome, ingredientes;
+    private String nomePrato, ingredientes;
 
-    Scanner digaPrato = new Scanner(System.in);
+    Scanner digaComposicaoPrato = new Scanner(System.in);
 
     ArrayList <Prato> listaPratos = new ArrayList<>();
 
 
-    public Prato(String nome, String ingredientes) {
-        this.nome = nome;
+    public Prato(String nomePrato, String ingredientes) {
+        this.nomePrato = nomePrato;
         this.ingredientes = ingredientes;
     }
 
+    //public Prato(){}
 
     public void adicionarPrato(){
+        System.out.println("Informe nome do Prato para novo cardápio: ");
+        nomePrato = digaComposicaoPrato.nextLine();
 
+        System.out.println("Informe quais ingredientes tem este prato: ");
+        ingredientes = digaComposicaoPrato.nextLine();
+
+        System.out.println("O nome do Prato cadastrado no cardápio é: "+nomePrato+",\nIngredientes: "+ingredientes);
+        Prato incluiPrato = new Prato(nomePrato,ingredientes);
+        listaPratos.add(incluiPrato);
 
     }
 
     public void exibirPratos(){
 
+        System.out.println("A lista de pratos no cardápio tem o total de " + listaPratos.size() + " item(s).");
+        for (int i = 0; i < listaPratos.size(); i++) {
+            System.out.println("Produto (" + listaPratos.get(i).getNomePrato() + ")\nIngredientes: " + listaPratos.get(i).getIngredientes());
+            System.out.println();
+        }
 
     }
 
     public void excluirPrato(){
+        System.out.println("Informe qual o nome do Prato que deseja excluir do cardápio: ");
+        System.out.println();
+        exibirPratos();
 
+        String excluindo = digaComposicaoPrato.nextLine();
+
+        for (int i = 0; i < listaPratos.size(); i++) {
+
+            if (listaPratos.get(i).getNomePrato().equals(excluindo)) {
+                listaPratos.remove(i);
+            }
+
+        }
 
     }
 
-    public String getNome() {
-        return nome;
+    public String getNomePrato() {
+        return nomePrato;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNomePrato(String nomePrato) {
+        this.nomePrato = nomePrato;
     }
 
     public String getIngredientes() {
